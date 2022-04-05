645c5597e6fe303c332ad598bc944cbc5012ce06
507cardoze <anthonycardoze@gmail.com>
2022-04-05 11:46:14 -0500

    Subida inicial

---
+++ src/com/anthony/Main.java
@@ -0,0 +1,13 @@
+package com.anthony;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+
+//        2. Diseñe tres clases  Cir, Esfe, Cilinc con atributos protegidos:
+//        ofrezca un menú que calcule el área de un círculo, esfera o de
+//        un cilindro cerrado, Aplique herencia.
